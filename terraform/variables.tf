eff7b13b22cfde9c0daec4407fbb8332b5815388
rickardgarnau-byte <rickardgarnau@gmail.com>
2026-08-30 11:46:08 +0200

    feat: setup terraform structure

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {
+    default = "FastlyDep"
+    type = string
+}
+
+variable "location" {
+  type    = string
+  default = "swedencentral"
+}
+
+variable "project_name" {
+  default = "FastlyDep"
+}
+
+variable "acr_name" {
+  default = "acrrickardgarnau"
+}
+
+variable "image_tag" {
+  default = "latest"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+  environment = "dev"
+  project     = "eclipsebord"
+  owner       = "rickard garnau"
+  }
+}
