8dcd81668a574cd0472589f75b7c8d483b0e8077
rickardgarnau-byte <rickardgarnau@gmail.com>
2026-08-31 14:14:04 +0200

    fix: convert total eclipse duration to numeric

--- backend/src/backend/api.py
+++ backend/src/backend/api.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from backend.data_processing import df_solar, df_lunar
+import json
 
 app = FastAPI()
 
 @app.get("/lunar/data")
 async def get_lunar_data():
-    return df_lunar.to_dict(orient="records")
+    return json.loads(df_lunar.to_json(orient="records"))
 
 @app.get("/solar/data")
 async def get_solar_data():
-    return df_lunar.to_dict(orient="records")
+    return json.loads(df_solar.to_json(orient="records"))
--- backend/src/backend/data_processing.py
+++ backend/src/backend/data_processing.py
@@ -2,4 +2,9 @@ import pandas as pd
 from backend.constants import DATA_DIRECTORY
 
 df_lunar = pd.read_csv(DATA_DIRECTORY / "lunar.csv")
+
+df_lunar["Total Exlipse Duration (m)"] = pd.to_numeric(
+    df_lunar["Total Eclipse Duration (m)"], errors="coerce"
+)
+
 df_solar = pd.read_csv(DATA_DIRECTORY / "solar.csv")
