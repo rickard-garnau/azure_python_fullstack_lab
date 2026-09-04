237a4d0a9c5a5684834268fa25438b650dfe2316
Rickard Garnau <rickard.garnau89@gmail.com>
2026-09-04 09:47:34 +0200

    chore: add comments

--- backend/src/backend/api.py
+++ backend/src/backend/api.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI
-from backend.data_processing import df_solar, df_lunar
 import json
+
+from fastapi import FastAPI
+
+from backend.data_processing import df_lunar, df_solar
 
 app = FastAPI()
 
+
 @app.get("/lunar/data")
 async def get_lunar_data():
-    return json.loads(df_lunar.to_json(orient="records")) # "to_json" instead of "to_dict" to automatically convert NaN to Null to avoid ValueError
+    return json.loads(
+        df_lunar.to_json(orient="records")
+    )  # "to_json" instead of "to_dict" to automatically convert NaN to Null to avoid ValueError releated to FastAPIs standard "allow_nan=False"
+
 
 @app.get("/solar/data")
 async def get_solar_data():
